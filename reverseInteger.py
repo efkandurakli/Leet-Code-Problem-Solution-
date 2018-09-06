38271f8df8ad60c3af6405d9abbf902f775bb333
Efkan Duraklı <efkandurakli@users.noreply.github.com>
2018-09-06 15:48:28 +0300

    Add files via upload
    reverse integer problem solution is added

---
+++ reverseInteger.py
@@ -0,0 +1,41 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+
+        if (x < -2147483648 or x > 2147483647):
+            return 0;
+
+
+        reverse = 0;
+        negative = False
+
+        if (x < 0):
+            negative = True
+            x = -1*x
+
+        while (x > 0):
+            lastDigit = x % 10
+            reverse = (reverse*10) + lastDigit
+            x = (int) (x / 10)
+
+
+        if (reverse < -2147483648 or reverse > 2147483647):
+            return 0;
+
+
+        if (negative):
+            reverse = -1*reverse
+        return reverse
+
+
+
+
+
+
+
+sol = Solution()
+
+print(sol.reverse(1534236469))
